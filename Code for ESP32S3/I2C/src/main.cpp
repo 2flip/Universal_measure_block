1b66dec7bee12aa0b5af6d7cc285462cf6cafbff
2flip <165209126+2flip@users.noreply.github.com>
2026-04-11 15:20:10 +0400

    Мелкие исправления репозитория

--- Code for ESP32S3/I2C/src/main.cpp
+++ Code for ESP32S3/I2C/src/main.cpp
@@ -1,18 +1,6 @@
 #include <Arduino.h>
+#include<Wire.h>
 
-// put function declarations here:
-int myFunction(int, int);
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+//I2C device found at address 0x68
+//I2C device found at address 0x7E
