9cd3c20105ca489755f63336deafe21e8578c9ac
2flip <165209126+2flip@users.noreply.github.com>
2026-04-16 03:11:52 +0400

    Исправлена распиновка I2C, из-за которой возникала нестабильное соединение с датчиком + добавлены куски кода с прерыванием датчика. Присутствует код для работы с светодиодом на есп

--- Code for ESP32S3/I2C/src/main.cpp
+++ Code for ESP32S3/I2C/src/main.cpp
@@ -1,30 +1,41 @@
-#include <Arduino.h>
-#include<Wire.h>
-#include<FS.h>
-#include<SD.h>
-#include<SPI.h>
+#include    <Arduino.h>
+#include    <Wire.h>
+//#include    <FS.h>
+//#include    <SD.h>
+//#include    <SPI.h>
+//#include    <Adafruit_NeoPixel.h>
 
+//Adafruit_NeoPixel LED_RGB(1, 48, NEO_GRBW + NEO_KHZ800);
 
-#define ICM_0       0x68    //addres icm_0
-#define ICM_1               //addres icm_1   
-#define SCL         16
-#define SDA         17
-#define INT_0       18
-#define INT_1
+//ICM pins
+#define ICM_0_ADD   0x68    //addres icm_0
+#define ICM_1_ADD           //addres icm_1   
+#define ICM_SCL     4      //correct problems with 
+#define ICM_SDA     5      //
+#define ICM_INT_0   12
+#define ICM_INT_1
 
-
+//SD card pins
 #define SD_CS       39
 #define SD_MISO     37
 #define SD_MOSI     35
 #define SD_CLK      36
 
 
-File dataFile;
+bool newICM42688Data = false;
+ 
+// File dataFile;
 
-SPIClass sd_spi(HSPI);
-SPISettings setting(1000000,  MSBFIRST,SPI_MODE0);
+// SPIClass sd_spi(HSPI);
+// SPISettings setting(1000000,  MSBFIRST,SPI_MODE0);
 
-uint8_t reg{}, accel_setup{};
+uint8_t reg{}, accel_setup{}, temp{};
+
+void myinthandler1()
+{
+  newICM42688Data = true;
+}
+
 
 
 //Addr_Device = Device Address; Reg = Register; N = Numbers of byte, but size is less then 32 bytes
@@ -35,7 +46,7 @@ uint8_t readRegisterI2C(uint8_t Addr_Device, uint8_t Reg, uint8_t N){
     Wire.write(Reg);
 
     //check error transmissiobn
-    res = Wire.endTransmission(false);
+    res = Wire.endTransmission(true);
 
     delay(10);
  
@@ -77,60 +88,105 @@ struct dataAccel{
 
 dataAccel packet0, packet1;
 
+
+uint8_t ICM_DRstatus(){
+    uint8_t temp = readRegisterI2C(ICM_0_ADD, 0x2D,1);
+    return temp;
+
+}
+
+
 //after succesfully write data on sd card we will be wire.end();
 
 void setup(){
     Serial.begin(115200);
 
+    // LED_RGB.begin();
+    // LED_RGB.setBrightness(100);
+    // LED_RGB.setPixelColor(0, uint32_t(LED_RGB.Color(244, 255, 29)));
+    // LED_RGB.show();
+
+    pinMode(ICM_INT_0, INPUT);//interrupt pins
+    attachInterrupt(ICM_INT_0, myinthandler1, RISING);
+
+
     //setup I2C
-    Wire.begin(SDA, SCL);//Wire.begin(SDA, SCL)
+    Wire.begin(ICM_SDA, ICM_SCL);//Wire.begin(SDA, SCL)
     Wire.setClock(1000*1000); //1MHz
 
     //check whoami icm
     Serial.print("Check ICM:    ");
-    Serial.println(readRegisterI2C(ICM_0, 0x75, 1));
+    Serial.println(readRegisterI2C(ICM_0_ADD, 0x75, 1));
 
 
     //set ODR 1KHz
-    reg = readRegisterI2C(ICM_0, 0x50, 1);
+    reg = readRegisterI2C(ICM_0_ADD, 0x50, 1);
     reg = 0x06 | (reg & 0xF0);
-    writeRegisterI2C(ICM_0, 0x50, reg);
+    writeRegisterI2C(ICM_0_ADD, 0x50, reg);
 
+    
     //Check ODR setting
-    accel_setup = readRegisterI2C(ICM_0, 0x50, 1);
+    accel_setup = readRegisterI2C(ICM_0_ADD, 0x50, 1);
     accel_setup = (accel_setup & 0x0F); 
     Serial.println(accel_setup, BIN);
 
+
     //set FS +-16 g
-    reg = readRegisterI2C(ICM_0, 0x50, 1);
+    reg = readRegisterI2C(ICM_0_ADD, 0x50, 1);
     reg = (0x00 <<5) | (reg & 0x1F);
-    writeRegisterI2C(ICM_0, 0x50, reg);
+    writeRegisterI2C(ICM_0_ADD, 0x50, reg);
+
 
     //check FS setting
-    accel_setup = readRegisterI2C(ICM_0, 0x50, 1);
+    accel_setup = readRegisterI2C(ICM_0_ADD, 0x50, 1);
     accel_setup = (accel_setup & 0xE0)>>5;
     Serial.println(accel_setup, BIN);
 
 
+    //turn low noise mode on accel
+    writeRegisterI2C(ICM_0_ADD,0x4E, 0x0F);
+
+    //push-pull, pulsed, active HIGH interrupts  
+    // writeRegisterI2C(ICM_0_ADD, 0x14,0x18 | 0x03);
+    // temp = readRegisterI2C(ICM_0_ADD,0x64, 1);
+    // writeRegisterI2C(ICM_0_ADD, 0x64,temp&~(0x10));
+    // writeRegisterI2C(ICM_0_ADD, 0x65, 0x08);
+
+
+
+
+
+
+
 
     //init sd card
-    sd_spi.begin(SD_CLK, SD_MISO, SD_MOSI, SD_CS);
-	if(!SD.begin(SD_CS, sd_spi)){
-		Serial.println("SD card not work!");
-		while(1);
-	}
-	dataFile = SD.open("/data.bin", FILE_WRITE);
-	if (!dataFile) {
-		Serial.println("file faild");
-		while(1);
-	
-	}
+    // sd_spi.begin(SD_CLK, SD_MISO, SD_MOSI, SD_CS);
+    // if(!SD.begin(SD_CS, sd_spi)){
+    //     Serial.println("SD card not work!");
+    //     while(1);
+    // }
+    // dataFile = SD.open("/data.bin", FILE_WRITE);
+    
+    // if (!dataFile) {
+    //     Serial.println("file faild");
+    //     while(1);
+    // }
     
     
     
     //enable icm
-    writeRegisterI2C(ICM_0, 0x4E, 0x0F);
+    writeRegisterI2C(ICM_0_ADD, 0x4E, 0x0F);
 
+    // LED_RGB.setPixelColor(0, uint32_t(LED_RGB.Color(0, 89, 255)));
+    // LED_RGB.show();
+
+
+    // //ICM_DRstatus();
+    if(newICM42688Data = true){
+        Serial.println("INTERRUPT WORK !!!!!");
+    }
+    
+    // RUSSIA();
 
 
 
